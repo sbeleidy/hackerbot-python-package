c1c8ecce9bf9fca0efd9ddf399d306970a94755e
Allen Chien <allen71090@gmail.com>
2025-02-27 19:32:01 -0700

    init commit

---
+++ main_controller.py
@@ -0,0 +1,34 @@
+import serial
+import threading
+
+class MainController:
+    def __init__(self):
+        port = '/dev/ttyACM0'
+        self.ser = serial.Serial(port=port, baudrate=230400)
+        self.read_thread = threading.Thread(target=self.read_serial)
+        self.read_thread.daemon = True
+        self.read_thread.start()
+
+    def get_board_and_port(self):
+        port = "/dev/ttyACM0"  # Adjust based on your Arduino's connection
+        board = "adafruit:samd:adafruit_qt_py_m0"  # Replace with your board type
+        return board, port
+
+    def send_raw_command(self, command):
+        if self.ser.is_open:
+            command = command + "\r\n"
+            self.ser.write(command.encode('utf-8')+b'\r')
+            # print(f"Sent: {command.strip()}")
+        else:
+            print("Error: Serial port is closed!")
+
+
+    def read_serial(self):
+        while True:
+            if self.ser.in_waiting > 0:
+                response = self.ser.readline().decode('utf8').strip()
+                print(response)
+
+    def disconnect(self):
+        self.ser.close()
+
---
+++ programmed_controller.py
@@ -0,0 +1,44 @@
+from main_controller import MainController
+
+class ProgrammedController(MainController):
+    def __init__(self):
+        super().__init__()
+        self.board, self.port = super().get_board_and_port()
+
+    def get_ping(self):
+        super().send_raw_command("PING")
+        return True
+
+    def get_versions(self):
+        super().send_raw_command("VERSION")
+        return True
+
+    def init_driver(self):
+        super().send_raw_command("INIT")
+        return True
+
+    def halt_driver(self):
+        super().send_raw_command("BUMP")
+        return True
+
+    def move(self, l_vel, a_vel):
+        command = f"MOVE,{l_vel},{a_vel}"
+        super().send_raw_command(command)
+        return True
+
+    def quickmap(self):
+        super().send_raw_command("QUICKMAP")
+        return True
+
+    def dock(self):
+        super().send_raw_command("DOCK")
+        return True
+
+    def leave_base(self):
+        super().send_raw_command("ENTER")
+        return True
+
+    def goto_pos(self, x_coord, y_coord, angle, speed):
+        command = f"GOTO,{x_coord},{y_coord},{angle},{speed}"
+        super().send_raw_command(command)
+        return True
