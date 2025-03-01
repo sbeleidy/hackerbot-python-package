71ed281a7b5693ae1701681206ea1eb6ce1c107f
Allen Chien <allen71090@gmail.com>
2025-02-28 20:24:33 -0700

    write cleaner code

--- bobby/navigate_robot.py
+++ bobby/navigate_robot.py
@@ -5,8 +5,11 @@ class Navigator:
     def __init__(self):
         self.controller = hhp.ProgrammedController()
 
+        self.vel = 0.4
+        self.exec_time = 20
+        self.configured = False
         # Set up location dict manually. ENHANCE VERSION: allow user to dynamically add locations from UI
-        self.LOCATION_DICT = {"Allen's room":(1,0,0,0.1), "Ian's room":(1,2,0,0.1), "Kitchen":(0,2,0,0.1), "Living room":(0,0,0,0.1)}
+        self.LOCATION_DICT = {"Allen's room":(1,0,0,self.vel), "Ian's room":(1,2,0,self.vel), "Kitchen":(0,2,0,self.vel), "Charger":(0,0,0,self.vel)}
 
         self.current_location = None
 
@@ -16,8 +19,13 @@ class Navigator:
         self.controller.init_driver()
         self.controller.leave_base()
         time.sleep(5)
+        self.static_navigation("Charger")
+        time.sleep(5)
+        self.configured = True
 
     def dynamic_navigation(self, string_command):
+        if not self.configured:
+            self.configure_navigation()
         string_command = string_command.lower().strip()
 
         # Handle docking command
@@ -37,12 +45,17 @@ class Navigator:
         print("Unknown command. Please specify a valid destination.")
 
     def static_navigation(self, destination):
+        if not self.configured:
+            self.configure_navigation()
+
+        print("Navigating to " + destination + ", current location: " + navigator.current_location + ", position: " + navigator.get_curr_pos())
         if destination == "Docking pose":
             self.controller.dock()
             self.current_location = "Docking station"
         else:
             self.controller.goto_pos(self.LOCATION_DICT[destination][0], self.LOCATION_DICT[destination][1], self.LOCATION_DICT[destination][2], self.LOCATION_DICT[destination][3])
             self.current_location = destination
+        time.sleep(self.exec_time)
 
     def get_curr_pos(self):
         return str(self.LOCATION_DICT[self.current_location])
@@ -51,24 +64,11 @@ def main():
     navigator = Navigator()
     exec_time = 20
     try:
-        for i in range(1):
-            navigator.configure_navigation()
+        for i in range(2):
             navigator.static_navigation("Allen's room")
-            print(f"Execute for {exec_time} seconds...")
-            time.sleep(exec_time)
-            print("In Allen's room, position: " + navigator.get_curr_pos())
             navigator.static_navigation("Ian's room")
-            print(f"Execute for {exec_time} seconds...")
-            time.sleep(exec_time)
-            print("In Ian's room, position: " + navigator.get_curr_pos())
             navigator.static_navigation("Kitchen")
-            print(f"Execute for {exec_time} seconds...")
-            time.sleep(exec_time)
-            print("In the kitchen, position: " + navigator.get_curr_pos())
-            navigator.static_navigation("Living room")
-            print(f"Execute for {exec_time} seconds...")
-            time.sleep(exec_time)
-            print("In the living room, position: " + navigator.get_curr_pos())
+            navigator.static_navigation("Charger")
 
     except Exception as e:
         print("Exception occurred: ", e)
