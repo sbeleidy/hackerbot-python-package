79094b66d960038a9b7f413a335455418bedbf56
Allen Chien <allen71090@gmail.com>
2025-03-08 22:39:17 -0700

    fix setup.py

--- hackerbot_modules/setup.py
+++ hackerbot_modules/setup.py
@@ -3,5 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name="hackerbot_helper",
     version="0.1",
-    packages=find_packages()
+    packages=find_packages(),
+    install_requires=[
+        "pyserial",
+    ],
+
 )
