bc4e1f855d4e30dcb10e03ff7f74102dbc9f7dc4
Allen Chien <allen71090@gmail.com>
2025-03-18 17:36:17 -0600

    add tests

--- hackerbot_modules/build/lib/hackerbot_helper/programmed_controller.py
+++ hackerbot_modules/build/lib/hackerbot_helper/programmed_controller.py
@@ -223,40 +223,46 @@ class ProgrammedController(MainController):
             return None
         
 
-    def move_head(self, angle):
+    # // float: yaw (rotation angle between 100.0 and 260.0 degrees - 180.0 is looking straight ahead)
+    # // float: pitch (vertical angle between 150.0 and 250.0 degrees - 180.0 is looking straight ahead)
+    # // Example - "LOOK,180.0,180.0"
+    def move_head(self, yaw, pitch, speed):
         try:
             self.check_head_control()
-            super().send_raw_command(f"MOVEHEAD,{angle}")
+            super().send_raw_command(f"H_LOOK, {yaw}, {pitch}, {speed}")
             # Not fetching json response since machine mode not implemented
             return True
         except Exception as e:
             self.log_error(f"Error in move_head: {e}")
             return False
         
-    def move_head_pitch(self, angle):
+    def enable_idle_mode(self):
         try:
             self.check_head_control()
-            super().send_raw_command(f"TILTHEAD,{angle}")
-            # Not fetching json response since machine mode not implemented
-            return True
-        except Exception as e:
-            self.log_error(f"Error in tilt_head: {e}")
-            return False
-        
-    def set_idle_mode(self):
-        try:
-            self.check_head_control()
-            super().send_raw_command("IDLE")
+            super().send_raw_command("H_IDLE, 1")
             # Not fetching json response since machine mode not implemented
             return True
         except Exception as e:
             self.log_error(f"Error in set_idle_mode: {e}")
             return False
         
+    def disable_idle_mode(self):
+        try:
+            self.check_head_control()
+            super().send_raw_command("H_IDLE, 0")
+            # Not fetching json response since machine mode not implemented
+            return True
+        except Exception as e:
+            self.log_error(f"Error in set_idle_mode: {e}")
+            return False
+        
+    # float: x (position between -1.0 and 1.0)
+    # float: y (position between -1.0 and 1.0)
+    # // Example - "H_GAZE,-0.8,0.2"
     def set_gaze(self, x, y):
         try:
-            self.check_system()
-            super().send_raw_command(f"GAZE,{x},{y}")
+            self.check_head_control()
+            super().send_raw_command(f"H_GAZE,{x},{y}")
             # Not fetching json response since machine mode not implemented
             return True
         except Exception as e:
