d5c13ec52897e48bea55a1deb64d3e334761c03b
Allen Chien <allen71090@gmail.com>
2025-03-20 20:32:11 -0600

    setup lazysusan

--- examples/playground.py
+++ examples/playground.py
@@ -1,31 +1,25 @@
 import hackerbot_helper as hhp
 import time
+from lazy_susan import LazySusan
 
 def main():
     try:
         # robot = hhp.ProgrammedController(port="/dev/ttyACM1", board="adafruit:samd:adafruit_qt_py_m0", verbose_mode=True)  
-        robot = hhp.ProgrammedController(verbose_mode=True)      
-        time.sleep(1)
-        print("Initializing driver...")
+        robot = hhp.ProgrammedController(verbose_mode=True)    
         robot.init_driver()
         print(robot.activate_machine_mode())
         print(robot.get_ping())
         print(robot.get_versions())
-        print(robot.leave_base())
-        time.sleep(10)
-        print(robot.move(l_vel=100, a_vel=100))
-        # robot.move_head(yaw=180, pitch=180, speed=2)
+        robot.leave_base()
+        time.sleep(1)
+        lazy_susan = LazySusan(robot)
+        lazy_susan.look_around()
+        robot.move(0,65)
+        time.sleep(1)
+        lazy_susan.look_around()
+        robot.move(0,65)
+        time.sleep(1)
 
-        # robot.disable_idle_mode()
-        # time.sleep(1)
-        # print("Getting map list...")
-        # map_list = robot.get_map_list()
-        # print("Map list:", map_list)
-        # time.sleep(1)
-        # print("Getting map...")
-        # map_data = robot.get_map(map_list[0])
-        # print("Map data:", map_data)
-        # time.sleep(1)
     except Exception as e:
         print("Error in main: ", e)
     finally:
