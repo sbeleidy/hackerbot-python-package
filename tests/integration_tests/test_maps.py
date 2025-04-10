3ccfceed5e97c544e89a6260062dd66b96d21333
Allen Chien <allen71090@gmail.com>
2025-04-09 22:20:09 -0600

    just testing

--- tests/integration_tests/test_maps.py
+++ tests/integration_tests/test_maps.py
@@ -4,16 +4,9 @@ import time
 def main():
     try:
         bot = Hackerbot(verbose_mode=True)
-        # print(bot.base.maps.list())
-        # print(bot.base.maps.fetch(14))
-        # bot.base.maps.goto(0, 0, 0, 0.1)
-        bot.base.maps.goto(1, 1, 0, 0.4)
-        bot.base.maps.goto(0.5, 0.5, 180, 0.4)
-        # for i in range(200):
-        #     print(bot.base.maps.position())
-        #     bot.base.maps._calculate_position_offset()
-        #     time.sleep(0.5)
-        # bot.base.maps._wait_until_reach_pose()
+        for _ in range(5):
+            bot.base.maps.goto(1, 1, 0, 0.4)
+            bot.base.maps.goto(0.5, 0.5, 180, 0.4)
 
 
     except Exception as e:
